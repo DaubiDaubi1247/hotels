f2c1f490f4eff486ccf93a75fd3ecb7c9b32ebce
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-01-18 09:54:26 +0300

    dev city serivce impl

--- src/main/java/ru/alex/hotels/repositories/CityRepository.java
+++ src/main/java/ru/alex/hotels/repositories/CityRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.alex.hotels.entitys.CityEntity;
 
+import java.util.Optional;
+
 @Repository
 public interface CityRepository extends JpaRepository<CityEntity, Long> {
+    Optional<CityEntity> findByName(String name);
+    Optional<CityEntity> findByIndex(String index);
 }
--- src/main/java/ru/alex/hotels/services/impl/CityServiceImpl.java
+++ src/main/java/ru/alex/hotels/services/impl/CityServiceImpl.java
@@ -1,11 +1,19 @@
 package ru.alex.hotels.services.impl;
 
+import ru.alex.hotels.repositories.CityRepository;
 import ru.alex.hotels.services.CityService;
 import ru.alex.hotels.tdo.City;
 
 public class CityServiceImpl implements CityService {
+    private CityRepository cityRepository;
+
+    public CityServiceImpl(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
+
     @Override
     public City createCity(City city) {
-        
+        if (cityRepository.findByName(city.getName()).isPresent())
+
     }
 }
