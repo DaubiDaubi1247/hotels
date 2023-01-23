c0a71e5d29861ae0da7e924ba50759b74aabf1fa
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-01-23 14:31:56 +0300

    create addRoom test

--- src/test/java/ru/alex/hotels/services/impl/RoomServiceImplTest.java
+++ src/test/java/ru/alex/hotels/services/impl/RoomServiceImplTest.java
@@ -1,16 +1,21 @@
 package ru.alex.hotels.services.impl;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ru.alex.hotels.entitys.HotelEntity;
 import ru.alex.hotels.entitys.RoomEntity;
 import ru.alex.hotels.exceptions.HotelNotFoundException;
 import ru.alex.hotels.exceptions.RoomAlreadyExists;
 import ru.alex.hotels.mappers.RoomMapper;
+import ru.alex.hotels.repositories.HotelRepository;
 import ru.alex.hotels.repositories.RoomRepository;
 import ru.alex.hotels.tdo.Room;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -21,6 +26,8 @@ import static ru.alex.hotels.dataForTests.RoomDataTest.testRoom;
 class RoomServiceImplTest {
     @Mock
     RoomRepository roomRepository;
+    @Mock
+    HotelRepository hotelRepository;
 
     @InjectMocks
     RoomServiceImpl roomService;
@@ -31,11 +38,25 @@ class RoomServiceImplTest {
         Room resRoom = RoomMapper.INSTANCE.roomEntityToRoom(testRoom());
 
         when(roomRepository.save(any(RoomEntity.class))).thenReturn(testRoom());
+        when(hotelRepository.findById(eq(1L))).thenReturn(Optional.of(new HotelEntity()));
 
         Room room = roomService.addRoom(1L, resRoom);
 
         assertEquals(resRoom, room);
         verify(roomRepository, times(1)).save(any(RoomEntity.class));
+    }
+
+    @Test
+    public void testCreateRoomNotFoundHotel() {
+
+        Room resRoom = RoomMapper.INSTANCE.roomEntityToRoom(testRoom());
+
+        when(hotelRepository.findById(eq(1L))).thenReturn(Optional.empty());
+
+        Throwable thrown = assertThrows(HotelNotFoundException.class,
+                () -> roomService.addRoom(1L, resRoom));
+
+        Assertions.assertNotNull(thrown.getMessage());
     }
 
 }
