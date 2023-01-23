607023733907e3e029656bc9022520edab906d8c
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-01-23 13:13:17 +0300

    create addRoom endpoint for Roomcontroller

--- src/main/java/ru/alex/hotels/controllers/HotelController.java
+++ src/main/java/ru/alex/hotels/controllers/HotelController.java
@@ -3,6 +3,7 @@ package ru.alex.hotels.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import ru.alex.hotels.exceptions.CityNotFound;
 import ru.alex.hotels.exceptions.HotelAlreadyExists;
 import ru.alex.hotels.exceptions.HotelNotFoundException;
---
+++ src/main/java/ru/alex/hotels/controllers/RoomController.java
@@ -0,0 +1,29 @@
+package ru.alex.hotels.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.alex.hotels.exceptions.RoomAlreadyExists;
+import ru.alex.hotels.services.RoomService;
+import ru.alex.hotels.tdo.Room;
+
+@RestController
+@RequestMapping("/rooms")
+public class RoomController {
+    private final RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @PostMapping("/{hotelId}")
+    public ResponseEntity<?> createRoom(@PathVariable Long hotelId, Room room) {
+        try {
+            return ResponseEntity.ok(roomService.addRoom(hotelId, room));
+        } catch (RoomAlreadyExists e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
